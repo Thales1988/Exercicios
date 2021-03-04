d9b8c02dcc81ed22c3d7f205e9e9d334a945e1bf
thales <2thales8@gmail.com>
2021-03-03 22:34:24 -0300

    exec1 (123) finalizado

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,42 @@
-// pegar a carteira
-// ir ao mercado
+// Item 1
+
+/*
+colocar o sapato
+levantar do sofá
+pegar a carteira e a chave
+abrir a porta
+sair
+fechar a porta
+caminhar até o mercado
+*/
+
+
+
+// Item 2
+
+var a = Math.random()
+console.log(a)
+var b = Math.random()
+console.log(b)
+var c = a + b
+console.log("a soma é", + c)
+
+
+
+// Item 3
+
+/*
+obtenha uma lampada extra
+desligue a energia da lampada que será removida através do interruptor
+pegue uma escada e leve ao local logo abaixo da lampada a ser removida
+suba na escada
+se a lampada estiver em temperatura nao prejudicial pegue com a mao e a gire no sentido anti-horario até que se desprenda do bocal
+desça da escada e reserve a lampada removida em algum lugar seguro
+pegue a lampada a ser colocada e suba a escada
+coloque a lampada nmo bocal e gire no sentido horario até que ela encaixe no fundo do bocal
+desça da escada
+ligue a energia da lampada que foi colocada através do interruptor para testa-la
+se funcionou guarde a escada e descarte a lampada removida
+*/
+
+
