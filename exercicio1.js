fa2d439385c95d778fa988cf624e469701c209f8
thales <2thales8@gmail.com>
2021-03-03 23:24:48 -0300

    exec1 (123) finalizado

--- exercicio1.js
+++ exercicio1.js
@@ -26,17 +26,31 @@ console.log("a soma é", + c)
 // 3 - Faça um algoritmo para trocar uma lâmpada. Descreva com detalhes.
 
 /*
-obtenha uma lampada extra
-desligue a energia da lampada que será removida através do interruptor
-pegue uma escada e leve ao local logo abaixo da lampada a ser removida
-suba na escada
-se a lampada estiver em temperatura nao prejudicial pegue com a mao e a gire no sentido anti-horario até que se desprenda do bocal
-desça da escada e reserve a lampada removida em algum lugar seguro
-pegue a lampada a ser colocada e suba a escada
-coloque a lampada nmo bocal e gire no sentido horario até que ela encaixe no fundo do bocal
-desça da escada
-ligue a energia da lampada que foi colocada através do interruptor para testa-la
-se funcionou guarde a escada e descarte a lampada removida
+primeiro verifique o motivo da troca da lampada
+se o motivo for por defeito ou queima faça:
+    verifique se a mesma está desligada
+    se nao
+        verifique se o interruptor está funcionando corretamente
+    se nao
+        verifique se está chegando energia até a mesma
+    se a lampada estiver ao alcance das maos use uma ferramenta para medir a tensao que chega ao bocal
+        se nao
+            se for necessario subir use uma escada
+se mesmo assim ela nao apresentar o funcionamento correto ou precisar ser trocada independentemente de defeito ou queima faça:
+    obtenha a lampada a ser colocada
+    desligue a energia da lampada que será removida através do interruptor
+    pegue uma escada e leve ao local logo abaixo da lampada a ser removida
+    suba na escada
+    se a lampada estiver em temperatura nao prejudicial pegue com a mao e a gire no sentido anti-horario até que se desprenda do bocal
+    desça da escada e reserve a lampada removida em algum lugar seguro
+    pegue a lampada a ser colocada e suba a escada
+    coloque a lampada nmo bocal e gire no sentido horario até que ela encaixe no fundo do bocal
+    desça da escada
+    ligue a energia da lampada que foi colocada através do interruptor para testa-la
+se funcionou guarde a escada
+se o motivo da troca foi por defeito ou queima descarte a lampada removida
+    se nao
+        guarde
 */
 
 
